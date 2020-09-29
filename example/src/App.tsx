52d1692579e891fbd34fd9988959b4206d18ed3e
Ricardo Girardi <girardiricardo@outlook.com>
2020-09-29 14:49:14 -0300

    Adding example folder and dependencies

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "material-rollup";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Button>Text button</Button>
+    </>
+  );
+};
+
+export default App;
